ab795261feefb0acdce19d08fc27efd7e94e24fa
Claudio Palominos <clpalomi@users.noreply.github.com>
2025-11-02 21:01:59 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const SUPABASE_URL = 'https://jstkzbgypzwyjwxraioz.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzdGt6Ymd5cHp3eWp3eHJhaW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MjI4NzEsImV4cCI6MjA3NjI5ODg3MX0.TyFHy_K2qYbk5HB20oFnDSpVoibfgJUHq9JyXPKsrF8';
 
 // Optional: OAuth redirect back to this page
-export const OAUTH_REDIRECT_TO = `${location.origin}${location.pathname}`;
+export const OAUTH_REDIRECT_TO = `${location.origin}/work-stack/index.html`;
 
 // App copy / flags
 export const APP = {
